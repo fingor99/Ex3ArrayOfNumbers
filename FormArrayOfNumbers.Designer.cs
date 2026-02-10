87ab6b5f608ce8c3163f6bc996446108dea03ee6
Finlay Gordon <159448297+fingor99@users.noreply.github.com>
2026-02-10 20:30:14 +0000

    Locked the array values content
    Locked the array values content to guide the user to input the length of the array first

--- FormArrayOfNumbers.Designer.cs
+++ FormArrayOfNumbers.Designer.cs
@@ -39,6 +39,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.labelArrayCapacity = new System.Windows.Forms.Label();
             this.buttonSetArray = new System.Windows.Forms.Button();
+            this.labelArrayDesc = new System.Windows.Forms.Label();
+            this.labelArray = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericSetArrayLength)).BeginInit();
             this.SuspendLayout();
             // 
@@ -54,7 +56,7 @@
             // 
             // buttonSetArrayLength
             // 
-            this.buttonSetArrayLength.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonSetArrayLength.BackColor = System.Drawing.Color.LightSteelBlue;
             this.buttonSetArrayLength.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
             this.buttonSetArrayLength.Location = new System.Drawing.Point(166, 120);
             this.buttonSetArrayLength.Name = "buttonSetArrayLength";
@@ -104,7 +106,7 @@
             // 
             // buttonInputIntoArray
             // 
-            this.buttonInputIntoArray.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonInputIntoArray.BackColor = System.Drawing.Color.DarkGray;
             this.buttonInputIntoArray.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
             this.buttonInputIntoArray.Location = new System.Drawing.Point(413, 120);
             this.buttonInputIntoArray.Name = "buttonInputIntoArray";
@@ -157,11 +159,32 @@
             this.buttonSetArray.Text = "Set Array";
             this.buttonSetArray.UseVisualStyleBackColor = false;
             // 
+            // labelArrayDesc
+            // 
+            this.labelArrayDesc.AutoSize = true;
+            this.labelArrayDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.labelArrayDesc.Location = new System.Drawing.Point(24, 217);
+            this.labelArrayDesc.Name = "labelArrayDesc";
+            this.labelArrayDesc.Size = new System.Drawing.Size(54, 20);
+            this.labelArrayDesc.TabIndex = 11;
+            this.labelArrayDesc.Text = "Array:";
+            // 
+            // labelArray
+            // 
+            this.labelArray.AutoSize = true;
+            this.labelArray.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.labelArray.Location = new System.Drawing.Point(78, 218);
+            this.labelArray.Name = "labelArray";
+            this.labelArray.Size = new System.Drawing.Size(0, 20);
+            this.labelArray.TabIndex = 12;
+            // 
             // FormArrayOfNumbers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(860, 616);
+            this.Controls.Add(this.labelArray);
+            this.Controls.Add(this.labelArrayDesc);
             this.Controls.Add(this.buttonSetArray);
             this.Controls.Add(this.labelArrayCapacity);
             this.Controls.Add(this.label1);
@@ -194,6 +217,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label labelArrayCapacity;
         private System.Windows.Forms.Button buttonSetArray;
+        private System.Windows.Forms.Label labelArrayDesc;
+        private System.Windows.Forms.Label labelArray;
     }
 }
 
